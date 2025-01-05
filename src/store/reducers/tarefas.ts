776f13fc5451b0ca4f52e1549b3d0fb678db8727
VitorinoAlves <vitorino.17alexandre@gmail.com>
2025-01-05 17:17:24 -0300

    Adicionando a ação de concluir uma tarefa

--- src/store/reducers/tarefas.ts
+++ src/store/reducers/tarefas.ts
@@ -58,9 +58,23 @@ const tarefasSlice = createSlice({
       } else {
         state.itens.push(action.payload)
       }
+    },
+    alteraStatus: (
+      state,
+      action: PayloadAction<{ id: number; finalizado: boolean }>
+    ) => {
+      const indexTarefaEditar = state.itens.findIndex(
+        (t) => t.id === action.payload.id
+      )
+
+      if (indexTarefaEditar >= 0) {
+        state.itens[indexTarefaEditar].status = action.payload.finalizado
+          ? enums.Status.CONCLUIDA
+          : enums.Status.PENDENTE
+      }
     }
   }
 })
 
-export const { remover, editar, cadastrar } = tarefasSlice.actions
+export const { remover, editar, cadastrar, alteraStatus } = tarefasSlice.actions
 export default tarefasSlice.reducer
